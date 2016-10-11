e38c5304fec7c8c8cce6042649e451e65a789283
stevesdawg <shrivathsav.seshan@gmail.com>
2016-10-10 21:39:10 -0400

    Header is properly removed. Json is properly captured. But parsing fails.

--- ArduinoISSTracker/ArduinoISSTracker.ino
+++ ArduinoISSTracker/ArduinoISSTracker.ino
@@ -9,6 +9,7 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char server[] = "api.open-notify.org";
 byte ip[] = {192, 168, 0, 177};
+int jsonLength;
 EthernetClient client;
 
 
@@ -17,7 +18,7 @@ EthernetClient client;
  * Try connecting automatically using DHCP first. If fails, connect using specific ip address.
  */
 void ethernetInit() {
-    Serial.print("Initializing Ethernet... ");
+    Serial.println("Initializing Ethernet... ");
     if (!Ethernet.begin(mac)) {
         Serial.println("DHCP Failed. Will assign IP.");
         Ethernet.begin(mac, ip);
@@ -38,49 +39,110 @@ void connectClient() {
 
 char* callApiReadResponse() {
     if (client.connected()) {
-        char* request = "GET /iss-now.json HTTP/1.1\nHost: api.open-notify.org\nConnection: keep-alive";
-        client.println(request);
+        client.println("GET /iss-now.json HTTP/1.1");
+        client.println("Host: api.open-notify.org");
+        client.println("Connection: close");
+        client.println();
         client.flush();
     }
+    Serial.println("GET Request Sent!");
+    Serial.println("Waiting for Data...");
     while (!client.available()) {
         // wait for incoming data
     }
 
     /**
      * Read Response Header.
-     * Strip response header.s
-     * Build a string containing pure json.
+     * flag is "Length". Triggers the capture of the json length.
      */
-    char header[200];
-    uint8_t count = 0;
-    uint8_t lineCount = 0;
     bool headerOver = false;
-    while (client.available() && !headerOver && count < 200) {
+    char jsonLengthSubstring[4];
+    char* lengthString = "Length";
+    uint8_t charCount = 0;
+    while (client.available() && !headerOver) {
         char c = client.read();
         Serial.print(c);
-        if (c == '\n') {
-            if (lineCount == 1) {
-                headerOver = true;
-                count -= 2;
-            } else {
-                lineCount++;
-            }
-        } else {
-            lineCount = 0;
+        if (c == '{') {
+            headerOver = true;
         }
-        header[count] = c;
-        count++;
+        if (c == lengthString[charCount]) {
+            charCount++;
+            if (charCount == 6) {
+                c = client.read(); // ':'
+                Serial.print(c);
+                c = client.read(); // ' '
+                Serial.print(c);
+                jsonLengthSubstring[0] = client.read();
+                jsonLengthSubstring[1] = client.read();
+                jsonLengthSubstring[2] = client.read();
+                jsonLengthSubstring[3] = '\0';
+                Serial.print(jsonLengthSubstring[0]);
+                Serial.print(jsonLengthSubstring[1]);
+                Serial.print(jsonLengthSubstring[2]);
+            }
+        } else if (charCount != 0 && c != jsonLengthSubstring[charCount]) {
+            charCount = 0;
+        }
     }
-    char jsonLengthSubstring[4];
-    strncpy(jsonLengthSubstring, header + count - 2, 3);
-    int jsonLength = atoi(jsonLengthSubstring);
-    char* jsonResponse = malloc(jsonLength+1);
-    count = 0;
+
+    jsonLength = atoi(jsonLengthSubstring);
+    Serial.print("Length of Json is: "); Serial.println(jsonLength);
+    char* jsonResponse = (char*) malloc(jsonLength+1);
+    uint8_t count = 1;
+    jsonResponse[0] = '{';
+
+    // Store the JSON data
     while (client.available() && count < jsonLength) {
         jsonResponse[count] = client.read();
         count++;
     }
+    jsonResponse[jsonLength] = '\0';
+
+    // Consume the buffer.
+    while(client.available()) {
+        client.read();
+    }
     return jsonResponse;
+}
+
+std::vector<double> parseJson(char const* jsonResponse) {
+    Serial.println(jsonResponse);
+    StaticJsonBuffer<170> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(jsonResponse);
+
+    if (!root.success()) {
+        Serial.println("jsonParsing failed");
+    }
+
+    double satLat = root["iss_position"]["latitude"];
+    double satLong = root["iss_position"]["longitude"];
+    std::vector<double> latAndLong = {satLat, satLong};
+    return latAndLong;
+}
+
+void moveMotors(std::vector<double> latAndLong) {
+    ISSTracker tracker(33, 84);
+    std::vector<std::vector<double>> coords = tracker.satUnitVector(latAndLong[0], latAndLong[1]);
+    Serial.println();
+    Serial.print("X: ");
+    Serial.println(coords[0][0]);
+    Serial.print("Y: ");
+    Serial.println(coords[1][0]);
+    Serial.print("Z: ");
+    Serial.println(coords[2][0]);
+}
+
+void promptUserToProceed(const char* message) {
+    if (Serial.available()) {
+        while (Serial.available()) {
+            Serial.read();
+        }
+    }
+    Serial.println(message);
+    while (!Serial.available()) {}
+    while (Serial.available()) {
+        Serial.read();
+    }
 }
 
 void setup() {
@@ -94,6 +156,32 @@ void setup() {
 }
 
 void loop() {
+    Serial.println("Calling API ......");
+    char* jsonResponse = callApiReadResponse();
     
+    int i = 0;
+    while(jsonResponse[i] != '\0') {
+        Serial.print(jsonResponse[i]);
+        i++;
+    }
+
+    promptUserToProceed("Contintue to Parsing?");
+
+    std::vector<double> latAndLong = parseJson(jsonResponse);
+    Serial.print("Latitude: ");
+    Serial.println(latAndLong[0]);
+    Serial.print("Longitude: ");
+    Serial.println(latAndLong[1]);
+
+    promptUserToProceed("Continue to Motor Control?");
+
+    moveMotors(latAndLong);
+
+    promptUserToProceed("Call Api?");    
+
+    for (int i = 0; i < jsonLength; i++) {
+        free(jsonResponse[i]);
+    }
+    free(jsonResponse);
 }
 
